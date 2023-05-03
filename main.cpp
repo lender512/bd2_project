240887836c5bf4379f0468bd9fc1e0837ef71208
Lender512 <luis.berrospi@utec.edu.pe>
2023-05-03 01:15:01 -0500

    :sparkles: Finished delete

--- main.cpp
+++ main.cpp
@@ -3,24 +3,30 @@
 // load from https://www.kaggle.com/datasets/tamber/steam-video-games
 
 int main() {
-    std::string file_name = "movies.dat";
-    AvlTree<MovieRecord> avlTree(file_name);
+    std::string file_name = "games.dat";
+    AvlTree<GameRecord> avlTree(file_name);
     //ranges = [100, 200, 400, 800, 1600, 3200, 6400, 12800, 25600, 51200]
-//    avlTree.load_from_csv("movies.csv", 1000, '|');
+//    avlTree.load_from_csv("games.csv", 10, '|');
 //    avlTree.test_from_csv("movies.csv", 51201, '|', {100, 200, 400, 800, 1600, 3200, 6400, 12800, 25600, 51200});
-//    int title = 6037892;
+//    int title = 142032;
+    avlTree.delete_item("Counter-Strike: Global Offensive");
+
+//    int title = 3315550;
 //    char* title = "PUBG: BATTLEGROUNDS";
-//    auto a = avlTree.search_by_name(title);
-////    std::cout << a.id << " " << a.primaryTitle << " "<< a.year << " " << a.genres << std::endl;
+    auto a = avlTree.search_by_name("Counter-Strike: Global Offensive");
+//    std::cout << a.id << " " << a.primaryTitle << " "<< a.year << " " << a.genres << std::endl;
 //    std::cout << a.publisher << std::endl;
 //    std::cout << a.gameTitle << std::endl;
 //    std::cout << a.price << std::endl;
-
-    auto result = avlTree.search_by_name_range(100000, 150000);
+    char left[150] = "A";
+    char right[150] = "G";
+    auto result = avlTree.search_by_name_range(left, right);
     for (auto &i : result) {
-        std::cout << i.id << " " << i.primaryTitle << " "<< i.year << " " << i.genres << std::endl;
+//        std::cout << i.id << " " << i.primaryTitle << " "<< i.year << " " << i.genres << std::endl;
+        std::cout << i.publisher << " "  << i.gameTitle <<" " <<i.publisher << std::endl;
     }
+
 //    avlTree.print_debug();
-    avlTree.print_preorder();
+//    avlTree.print_preorder();
     return 0;
 }
